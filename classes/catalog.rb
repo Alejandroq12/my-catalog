b434948bad9344e68bd9de730d47cc98901ffc62
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-03 20:42:18 -0600

    Add class Catalog

--- classes/catalog.rb
+++ classes/catalog.rb
@@ -2,3 +2,12 @@ require_relative 'managers/book_manager'
 require_relative 'managers/game_manager'
 require_relative 'managers/music_manager'
 
+class Catalog
+  def initialize
+    @book_manager = BookManager.new
+    @game_manager = GameManager.new
+    @music_manager = MusicManager.new
+  end
+
+
+end
