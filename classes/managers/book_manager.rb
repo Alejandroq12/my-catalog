b9724f7f68e66a2b164117a6420de56d662ec332
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-03 21:11:07 -0600

    Add class BookManager

--- classes/managers/book_manager.rb
+++ classes/managers/book_manager.rb
@@ -3,3 +3,12 @@ require_relative '../models/label'
 require 'json'
 require 'date'
 require 'fileutils'
+
+class BookManager
+  attr_accessor :books, :labels
+
+  def initalize
+    @books = []
+    @labels = []
+  end
+end
