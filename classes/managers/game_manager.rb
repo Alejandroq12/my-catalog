ccf88576ca125e63db5c94635be0103e70fc7438
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-08-04 16:06:56 -0600

    Assing game and label id's and create new instances of game and label

--- classes/managers/game_manager.rb
+++ classes/managers/game_manager.rb
@@ -10,7 +10,7 @@ class GameManager
     @games = []
     @labels = []
   end
-
+  
   def add_game
     display_message('Enter the game title: ')
     title = gets.chomp
@@ -20,5 +20,10 @@ class GameManager
     publisher = gets.chomp
     display_message('Enter the game cover color: ')
     color = gets.chomp
-  end
+
+    game_id = generate_game_id
+    game = Game.new(id: game_id, title: title, publish_date: release_date, author: publisher, label: nil)
+
+    label_id = generate_label_id
+    label = Label.new(id: label_id, title: title, color: color)
 end
