351314bf3a4b6a55141054e6a2d6fd72bba046f6
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-08-04 15:27:57 -0600

    Required necessary files and libraries

--- classes/managers/game_manager.rb
+++ classes/managers/game_manager.rb
@@ -2,36 +2,3 @@ require_relative '../models/game'
 require_relative '../models/label'
 require 'json'
 require 'fileutils'
-
-class GameManager
-  attr_accessor :games, :labels
-
-  def initialize
-    @games = []
-    @labels = []
-  end
-
-  def add_game
-    display_message('Enter the game title: ')
-    title = gets.chomp
-    display_message('Enter the game\'s release date (YYYY-MM-DD): ')
-    release_date = gets.chomp
-    display_message('Enter the game publisher: ')
-    publisher = gets.chomp
-    display_message('Enter the game cover color: ')
-    color = gets.chomp
-
-    game_id = generate_game_id
-    game = Game.new(id: game_id, title: title, publish_date: release_date, author: publisher, label: nil)
-
-    label_id = generate_label_id
-    label = Label.new(id: label_id, title: title, color: color)
-
-    @games.push(game)
-    @labels.push(label)
-    display_message('Game added successfully.')
-    store_game(game)
-    store_label(label)
-  end
-
-end
